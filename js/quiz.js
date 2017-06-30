b366d096b713c1568c998be4454ea15bbb51d6bc
Peter-32 <peter@impactradius.com>
2017-06-29 19:54:44 -0700

    added questions right and wrong to appear

--- js/quiz.js
+++ js/quiz.js
@@ -2,6 +2,15 @@ function print(message) {
   document.write(message);
 }
 
+function printList(list) {
+html = "<ol>";
+for (var i = 0; i < list.length; i++) {
+	html += "<li>" + (list[i]) + "</li>";
+}
+html += "</ol>";
+print(html);
+}
+
 var guess;
 var numCorrect = 0;
 var questions = [
@@ -9,11 +18,16 @@ var questions = [
   ["What letter must appear at the beginning of the registration number of all non-military aircraft in the U.S.?", "N"],
   ["Who delivered the less famous two-hour speech that preceded Abraham Lincoln's two-minute Gettysburg Address?", "EDWARD EVERETT"]
 ];
+var questionsCorrect = [];
+var questionsWrong = [];
 
 for (var i = 0; i < questions.length; i++) {
     guess = prompt(questions[i][0]);
     if (guess.toUpperCase() === questions[i][1]) {
     	numCorrect++;
+    	questionsCorrect.push(questions[i][0]);    	
+    } else {
+    	questionsWrong.push(questions[i][0]);
     }
 }
 
@@ -21,7 +35,16 @@ print("<p>You got " + numCorrect + " answers correct out of " + questions.length
 if (numCorrect === 0) {
     print("<p>Please try again.</p>");
 } else if (numCorrect > 0 && numCorrect != questions.length) {
-	print("<p>Good job!  Feel free to play again.</p>")
+	print("<p>Good job!  Feel free to play again.</p>");
 } else if (numCorrect === questions.length) {
-	print("<p>You got a perfect score!  Great job!</p>")
+	print("<p>You got a perfect score!  Great job!</p>");
 }
+
+print("<p>You got these questions correct:</p>");
+printList(questionsCorrect);
+
+
+print("<p>You got these questions wrong:</p>");
+printList(questionsWrong);
+	
+	
